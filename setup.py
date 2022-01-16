ce5e4e0e1bfd150c674da24db72e46b02b207737
Yuhang Tao <yuhang.tao.email@gmail.com>
2022-01-16 14:25:48 +0800

    delete usless dependencies

--- setup.py
+++ setup.py
@@ -26,7 +26,13 @@ setup(
     # Keywords that define your package best
     keywords=['nlp', 'file', 'regex'],
     install_requires=[            # I get to this in a second
-        
+        "fitz",
+        "jsonlines",
+        "numpy",
+        "pandas",
+        "pdfplumber",
+        "Pillow",
+        "tqdm"
     ],
     classifiers=[
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
